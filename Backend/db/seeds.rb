8fa57ea003f102642397927c2a3151a94042d562
jamesabenj <james.abenj@gmail.com>
2020-08-25 15:17:37 -0400

    seed quiz data

--- Backend/db/seeds.rb
+++ Backend/db/seeds.rb
@@ -6,4 +6,234 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: 'test')
+Lesson.create(planet: "mercury")
+Question.create(
+    title: "Mercury's enormous core represents 85% of the planet's radius, and is made of ____",
+    "option1" => "Mercury",
+    "option2" => "Iron",
+    "option3" => "Titanium",
+    "option4" => "Lead",
+    answer: "Iron",
+    lesson_id: 1
+)
+Question.create(
+    title: "During the day, Mercury's surface temperature can reach ____ degrees Farenheit",
+    "option1" => "100",
+    "option2" => "30",
+    "option3" => "550",
+    "option4" => "800",
+    answer: "800",
+    lesson_id: 1
+)
+Question.create(
+    title: "At night, Mercury's surface temperature can can drop as low as ____ degrees Farenheit",
+    "option1" => "-10",
+    "option2" => "-290",
+    "option3" => "-150",
+    "option4" => "-400",
+    answer: "-290",
+    lesson_id: 1
+)
+
+Lesson.create(planet: "venus")
+Question.create(
+    title: "From Earth, Venus is the second brightest object in the night sky after ____",
+    "option1" => "Mars",
+    "option2" => "The Big Dipper",
+    "option3" => "The Moon",
+    "option4" => "The International Space Station",
+    answer: "The Moon",
+    lesson_id: 2
+)
+Question.create(
+    title: "Venus is named after the Roman god of  ____",
+    "option1" => "Love and Beauty",
+    "option2" => "War",
+    "option3" => "The Sky",
+    "option4" => "Time",
+    answer: "Love and Beauty",
+    lesson_id: 2
+)
+Question.create(
+    title: "Venus rotates in the opposite direction to most other planets. This rotation is reffered to as ____",
+    "option1" => "Retrograde",
+    "option2" => "Retrospin",
+    "option3" => "Antispin",
+    "option4" => "Rewind",
+    answer: "Retrograde",
+    lesson_id: 2
+)
+
+Lesson.create(planet: "earth")
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 3
+)
+Question.create(
+    title: "Earth is the ____ planet in the solar system",
+    "option1" => "Lightest",
+    "option2" => "Densest",
+    "option3" => "Smallest",
+    "option4" => "Oldest",
+    answer: "Densest",
+    lesson_id: 3
+)
+Question.create(
+    title: "Only about ____ % of Earth's water is fresh",
+    "option1" => "10",
+    "option2" => "30",
+    "option3" => "3",
+    "option4" => "1",
+    answer: "3",
+    lesson_id: 3
+)
+
+Lesson.create(planet: "mars")
+Question.create(
+    title: "A year on Mars is almost ____ as long as a year on Earth",
+    "option1" => "Twice",
+    "option2" => "Exactly",
+    "option3" => "Half",
+    "option4" => "A Third",
+    answer: "Twice",
+    lesson_id: 4
+)
+Question.create(
+    title: "Mars is home to the ____ in the solar system",
+    "option1" => "Oldest Alien",
+    "option2" => "Largest Moon",
+    "option3" => "Smallest Lake",
+    "option4" => "Tallest Mountain",
+    answer: "Tallest Mountain",
+    lesson_id: 4
+)
+Question.create(
+    title: "Mars is the only other planet besides Earth that has ____",
+    "option1" => "Polar Ice Caps",
+    "option2" => "Animals",
+    "option3" => "Moons",
+    "option4" => "Seasons",
+    answer: "Polar Ice Caps",
+    lesson_id: 4
+)
+
+Lesson.create(planet: "jupiter")
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 5
+)
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 5
+)
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 5
+)
+
+Lesson.create(planet: "saturn")
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 6
+)
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 6
+)
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 6
+)
+
+Lesson.create(planet: "uranus")
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 7
+)
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 7
+)
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 7
+)
+
+Lesson.create(planet: "neptune")
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 8
+)
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 8
+)
+Question.create(
+    title: "'Earth' is a Germanic word that means ____",
+    "option1" => "Water",
+    "option2" => "Third",
+    "option3" => "Ground",
+    "option4" => "Life",
+    answer: "Ground",
+    lesson_id: 8
+)
