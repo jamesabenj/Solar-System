4d16267e27a508089d1d2104c036ae2ccb60bc24
jamesabenj <james.abenj@gmail.com>
2020-09-02 15:03:52 -0400

    fix grading system

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,8 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-    fetchQuestions(1)
-    fetchQuestions(2)
-    fetchQuestions(3)
-    fetchQuestions(4)
+    fetchQuestions(lessonIds)
     createUser()
     dropdown()
 })
@@ -14,10 +11,17 @@ const mercuryLesson = document.getElementById('mercury-lesson')
 const venusLesson = document.getElementById('venus-lesson')
 const earthLesson = document.getElementById('earth-lesson')
 const marsLesson = document.getElementById('mars-lesson')
+const jupiterLesson = document.getElementById('jupiter-lesson')
+const saturnLesson = document.getElementById('saturn-lesson')
+const uranusLesson = document.getElementById('uranus-lesson')
+const neptuneLesson = document.getElementById('neptune-lesson')
 const grade = document.getElementById('grade')
 const gradeBook = document.getElementById('grade-book')
 const reset = document.getElementById('reset')
 let score = 0
+let lessonIds = [1,2,3,4,5,6,7,8]
+const current_user = new Object
+
 
 function createUser() {
     let usersForm = document.getElementById("user-form")
@@ -53,7 +57,11 @@ function userFormSubmission(){
         body: JSON.stringify(user)
     })
     .then(resp => resp.json())
-    .then(() => showLessons()) 
+    .then(data => {
+        current_user.username = username
+        current_user.id = data.id
+    })
+    .then(() => showLessons())
 }
 
 function dropdown() {
@@ -80,6 +88,28 @@ function showLessons() {
     planetSelect.classList.toggle('show')
     // planetSelect.style.display = 'flex'
     // planetSelect.style.width = '5000px'
+}
+
+function fetchQuestions(lessonIds){
+    lessonIds.forEach(id => {
+        if (id == 1) {
+            fetch(`${BASE_URL}/lessons/${id}/questions`).then(resp => resp.json()).then(json => renderMercuryQuestions(json))
+        }else if (id == 2) {
+            fetch(`${BASE_URL}/lessons/${id}/questions`).then(resp => resp.json()).then(json => renderVenusQuestions(json))
+        }else if (id == 3) {
+            fetch(`${BASE_URL}/lessons/${id}/questions`).then(resp => resp.json()).then(json => renderEarthQuestions(json))
+        }else if (id == 4) {
+            fetch(`${BASE_URL}/lessons/${id}/questions`).then(resp => resp.json()).then(json => renderMarsQuestions(json))
+        }else if (id == 5) {
+            fetch(`${BASE_URL}/lessons/${id}/questions`).then(resp => resp.json()).then(json => renderJupiterQuestions(json))
+        }else if (id == 6) {
+            fetch(`${BASE_URL}/lessons/${id}/questions`).then(resp => resp.json()).then(json => renderSaturnQuestions(json))
+        }else if (id == 7) {
+            fetch(`${BASE_URL}/lessons/${id}/questions`).then(resp => resp.json()).then(json => renderUranusQuestions(json))
+        }else if (id == 8) {
+            fetch(`${BASE_URL}/lessons/${id}/questions`).then(resp => resp.json()).then(json => renderNeptuneQuestions(json))
+        }
+    })
 }
 
 function renderMercuryQuestions(content) {
@@ -149,45 +179,73 @@ function renderMarsQuestions(content) {
     });
 }
 
-function fetchQuestions(lesson_id){
-    fetch(`${BASE_URL}/lessons/${lesson_id}/questions`).then(resp => resp.json()).then(function(json) {
-        if (lesson_id == 1) {
-            renderMercuryQuestions(json)
-        }else if (lesson_id == 2) {
-            renderVenusQuestions(json)
-        }else if (lesson_id == 3) {
-            renderEarthQuestions(json)
-        }else if (lesson_id == 4) {
-            renderMarsQuestions(json)
-        }
-        // }else if (lesson_id == 5) {
-        //     renderJupiterQuestions(json)
-        // }else if (lesson_id == 6) {
-        //     renderSaturnQuestions(json)
-        // }else if (lesson_id == 7) {
-        //     renderUranusQuestions(json)
-        // }else if (lesson_id == 8) {
-        //     renderNeptuneQuestions(json)
-        // }
-    })
+function renderJupiterQuestions(content) {
+    content.forEach(i => {
+        jupiterLesson.innerHTML += 
+        `
+        <h3>${i.title}</h3>
+        <form>
+            <input type="radio" name="jupiter-${i.id}" value="${i.option1}">${i.option1}</input><br>
+            <input type="radio" name="jupiter-${i.id}" value="${i.option2}">${i.option2}</input><br>
+            <input type="radio" name="jupiter-${i.id}" value="${i.option3}">${i.option3}</input><br>
+            <input type="radio" name="jupiter-${i.id}" value="${i.option4}">${i.option4}</input>
+            <input name="jupiter-${i.id}" type="hidden" value="${i.answer}"></input>
+        </form>
+
+        `
+    });
 }
 
+function renderSaturnQuestions(content) {
+    content.forEach(i => {
+        saturnLesson.innerHTML += 
+        `
+        <h3>${i.title}</h3>
+        <form>
+            <input type="radio" name="saturn-${i.id}" value="${i.option1}">${i.option1}</input><br>
+            <input type="radio" name="saturn-${i.id}" value="${i.option2}">${i.option2}</input><br>
+            <input type="radio" name="saturn-${i.id}" value="${i.option3}">${i.option3}</input><br>
+            <input type="radio" name="saturn-${i.id}" value="${i.option4}">${i.option4}</input>
+            <input name="saturn-${i.id}" type="hidden" value="${i.answer}"></input>
+        </form>
 
-grade.addEventListener('click', function(){
-    event.preventDefault()
-    gradeMercury()
-    gradeVenus()
-    gradeEarth()
-    gradeMars()
-    planetSelect.style.display = 'none'
-    gradeBook.innerHTML =
-    `
-    <h2>You Scored ${score} / 24</h2>
+        `
+    });
+}
 
-    `
-    gradeBook.classList.remove('hidden')
-    reset.style.display = "inline"
-})
+function renderUranusQuestions(content) {
+    content.forEach(i => {
+        uranusLesson.innerHTML += 
+        `
+        <h3>${i.title}</h3>
+        <form>
+            <input type="radio" name="uranus-${i.id}" value="${i.option1}">${i.option1}</input><br>
+            <input type="radio" name="uranus-${i.id}" value="${i.option2}">${i.option2}</input><br>
+            <input type="radio" name="uranus-${i.id}" value="${i.option3}">${i.option3}</input><br>
+            <input type="radio" name="uranus-${i.id}" value="${i.option4}">${i.option4}</input>
+            <input name="uranus-${i.id}" type="hidden" value="${i.answer}"></input>
+        </form>
+
+        `
+    });
+}
+
+function renderNeptuneQuestions(content) {
+    content.forEach(i => {
+        neptuneLesson.innerHTML += 
+        `
+        <h3>${i.title}</h3>
+        <form>
+            <input type="radio" name="neptune-${i.id}" value="${i.option1}">${i.option1}</input><br>
+            <input type="radio" name="neptune-${i.id}" value="${i.option2}">${i.option2}</input><br>
+            <input type="radio" name="neptune-${i.id}" value="${i.option3}">${i.option3}</input><br>
+            <input type="radio" name="neptune-${i.id}" value="${i.option4}">${i.option4}</input>
+            <input name="neptune-${i.id}" type="hidden" value="${i.answer}"></input>
+        </form>
+
+        `
+    });
+}
 
 function gradeMercury() {
     for(let x = 1; x < 4; x++) {
@@ -248,6 +306,98 @@ function gradeMars() {
         }    
     }
 }
+
+function gradeJupiter() {
+    for(let x = 13; x < 16; x++) {
+        let coll = document.querySelectorAll(`input[name="jupiter-${x}"]`)
+        let selectedValue
+        coll.forEach(i => {
+            if (i.checked) {
+                selectedValue = i.value
+            }
+        })
+        if (selectedValue == coll[4].value) {
+            score++ 
+        }    
+    }
+}
+
+function gradeSaturn() {
+    for(let x = 16; x < 19; x++) {
+        let coll = document.querySelectorAll(`input[name="saturn-${x}"]`)
+        let selectedValue
+        coll.forEach(i => {
+            if (i.checked) {
+                selectedValue = i.value
+            }
+        })
+        if (selectedValue == coll[4].value) {
+            score++ 
+        }    
+    }
+}
+
+function gradeUranus() {
+    for(let x = 19; x < 22; x++) {
+        let coll = document.querySelectorAll(`input[name="uranus-${x}"]`)
+        let selectedValue
+        coll.forEach(i => {
+            if (i.checked) {
+                selectedValue = i.value
+            }
+        })
+        if (selectedValue == coll[4].value) {
+            score++ 
+        }    
+    }
+}
+
+function gradeNeptune() {
+    for(let x = 22; x < 25; x++) {
+        let coll = document.querySelectorAll(`input[name="neptune-${x}"]`)
+        let selectedValue
+        coll.forEach(i => {
+            if (i.checked) {
+                selectedValue = i.value
+            }
+        })
+        if (selectedValue == coll[4].value) {
+            score++ 
+        }    
+    }
+}
+
+function setUserGrade(userId, grade) {
+    fetch(`${BASE_URL}/users/${userId}`, {
+    headers: { "Content-Type": "application/json;" },
+    method: 'PUT',
+    body: JSON.stringify({
+      username: `${current_user.username}`,
+      grade: `${grade}`,
+    })
+  })
+}
+
+grade.addEventListener('click', function(){
+    event.preventDefault()
+    gradeMercury()
+    gradeVenus()
+    gradeEarth()
+    gradeMars()
+    gradeJupiter()
+    gradeSaturn()
+    gradeUranus()
+    gradeNeptune()
+    setUserGrade(current_user.id, score)
+    planetSelect.style.display = 'none'
+    gradeBook.innerHTML =
+    `
+    <h2>${current_user.username} Scored ${score} / 24</h2>
+    <h4>
+    `
+    gradeBook.classList.remove('hidden')
+    reset.style.display = "inline"
+})
 
 reset.addEventListener('click', function(){
     location.reload()
